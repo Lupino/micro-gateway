80595dd9d9ab8f0a98bc8c62f28b0ee893fbd3be
Lupino <lmjubuntu@gmail.com>
2018-12-28 09:39:16 +0800

    inherit static

--- default.nix
+++ default.nix
@@ -11,6 +11,7 @@ let
   yuntan-common-signature       = haskellPackages.callPackage ./nix/yuntan-common-signature.nix {};
   yuntan-gateway = haskellPackages.callPackage ./nix/yuntan-gateway.nix {
     inherit yuntan-common-signature;
+    inherit static;
   };
 
 in {
